ea13bae1871c1e684c8cfe5388c415ffff968e7d
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-27 11:12:13 +0530

    setup express and mongodb

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.port || 4000;
+
+
+mongoose.connect(process.env.db_url)
+    .then(() => {
+        console.log("Connected to database");
+    }).catch((err) => {
+        console.log(err);
+    })
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`connected on port: ${port}`);
+});
