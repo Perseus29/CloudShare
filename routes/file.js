eb04b8d2bae15ebecdd97c4d4ee13f4154f251fe
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-30 11:13:43 +0530

    minor changes

--- routes/file.js
+++ routes/file.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination: (req,file,cb) => cb(null,'')
+})
 
 router.post('/' , (req,res)=>{
-    
+    if(!req.file){
+        return res.json({error:"Didn't recieve a file"})
+    }
 })
 
 module.exports.router;
