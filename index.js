1ed7b459da892dca356094a26348ea5d6a6ed849
Priyanshu <priyanshumahawar29@gmail.com>
2023-06-30 12:24:12 +0530

    setup download page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const app = express();
 
 const port = process.env.port || 4000;
 
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine' , 'ejs');
 
 mongoose.connect(process.env.db_url)
     .then(() => {
