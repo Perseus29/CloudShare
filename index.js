b999a8a0f644d7a9c7a7b8eca9a6c6d9b891b87a
Priyanshu <priyanshumahawar29@gmail.com>
2023-07-02 14:01:53 +0530

    updated routing

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path');
 require('dotenv').config();
 const app = express();
 const port = process.env.port || 4000;
- 
+
 app.use(express.json())
-app.set('views',path.join(__dirname,'/views'));
-app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 
@@ -18,12 +18,18 @@ mongoose.connect(process.env.db_url)
         console.log(err);
     })
 
-app.use('/api/files' , require('./routes/file'));
-app.use('/files',require('./routes/view'));
-app.use('/files/download',require('./routes/download'));
-app.use('/home' , (req,res)=>{
+
+app.get('/', (req, res) => {
     res.render('home');
 });
+app.use('/api/files', require('./routes/file'));
+app.use('/files', require('./routes/view'));
+app.use('/files/download', require('./routes/download'));
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+  });
+
 
 app.listen(port, () => {
     console.log(`Connected on port: ${port}`);
--- public/index.js
+++ public/index.js
@@ -5,51 +5,96 @@ const bgProgress = document.querySelector(".bg-progress");
 const percentDiv = document.querySelector("#percent")
 const progressBar = document.querySelector(".progress-bar");
 const processContainer = document.querySelector(".progress-container")
+const fileURLInput = document.querySelector("#fileURL");
+const copyBtn = document.querySelector("#copyBtn");
+const sharingContainer = document.querySelector(".sharing-container");
+const emailForm = document.querySelector("#emailForm");
+const maxAllowedSize = 100*1024*1024;
 
-dropZone.addEventListener("dragover",(e)=>{
+dropZone.addEventListener("dragover", (e) => {
     e.preventDefault();
     console.log("Worked!");
-    if(!dropZone.classList.contains("dragged")){
+    if (!dropZone.classList.contains("dragged")) {
         dropZone.classList.add("dragged");
-    }    
+    }
 });
 
-dropZone.addEventListener("dragleave",()=>{
+dropZone.addEventListener("dragleave", () => {
     dropZone.classList.remove("dragged");
 });
 
-dropZone.addEventListener("drop",(e)=>{
+dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
-    dropZone.classList.remove("dragged");    
+    dropZone.classList.remove("dragged");
     const files = e.dataTransfer.files;
     console.log(files);
-    if(files.length){
+    if (files.length) {
         fileInput.files = files;
         uploadFile();
     }
-    
+
 });
 
-fileInput.addEventListener("change" , ()=>{
+fileInput.addEventListener("change", () => {
     uploadFile();
 })
 
-browseBtn.addEventListener("click",()=>{
+browseBtn.addEventListener("click", () => {
     fileInput.click();
 })
 
-const uploadFile = ()=>{
+copyBtn.addEventListener("click", () => {
+    fileURLInput.select();
+    document.execCommand("copy");
+})
 
-    processContainer.style.display = "block";
+emailForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const url = fileURLInput.value;
+    const formData = {
+        uuid: url.split("/").splice(-1, 1)[0],
+        recieverEmail: emailForm.elements["to-email"].value,
+        senderEmail: emailForm.elements["from-email"].value
+    }
+    // console.log(formData);   
+
+    emailForm[2].setAttribute("disabled" , "true");
+
+    fetch("http://localhost:4000/api/files/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }).then(res => res.json())
+      .then(({ success }) => {
+        if(success){
+            sharingContainer.style.display = "none";
+        }
+    });
+});
+
+const uploadFile = () => {
+    if(fileInput.files.length>1){
+        fileInput.value = "";
+        alert("Upload only 1 file!")
+        return;
+    }
     const file = fileInput.files[0];
+    if(file.size > maxAllowedSize){
+        fileInput.value = "";
+        alert("Can't upload more than 100MB!")
+        return;
+    }
+    processContainer.style.display = "block";
     const formData = new FormData();
-    formData.append("uploadFile",file); 
+    formData.append("uploadFile", file);
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () =>{
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            console.log(xhr.response); 
-            showLink(JSON.parse(xhr.response));           
-        }        
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.response);
+            onUploadSuccess(JSON.parse(xhr.response));
+        }
     };
 
     xhr.upload.onprogress = function (event) {
@@ -59,15 +104,19 @@ const uploadFile = ()=>{
         const scaleX = `scaleX(${percent / 100})`;
         bgProgress.style.width = `${percent}%`;
         progressBar.style.transform = scaleX;
-      };
+    };
 
-    xhr.open("POST","http://localhost:4000/api/files");
+    xhr.open("POST", "http://localhost:4000/api/files");
     xhr.send(formData);
 
 }
 
-const showLink = ({file}) => {
-    console.log(file);    
+const onUploadSuccess = ({ file: url }) => {
+    console.log(url);
+    fileInput.value = "";
+    emailForm[2].removeAttribute("disabled");
     processContainer.style.display = "none";
+    sharingContainer.style.display = "block";
+    fileURLInput.value = url;
 }
 
